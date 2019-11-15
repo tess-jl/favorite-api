7428536d79bc724763c7a4b77854cedb40b0f3bc
Tess Lameyer <tjlameyer@gmail.com>
2019-11-15 08:58:03 -0800

    set up starter code

---
+++ data/create-tables.js
@@ -0,0 +1,34 @@
+const client = require('../lib/client');
+
+run();
+
+async function run() {
+
+    try {
+        // run a query to create tables
+        await client.query(`
+            CREATE TABLE users (
+                id SERIAL PRIMARY KEY,
+                email VARCHAR(256) NOT NULL,
+                hash VARCHAR(512) NOT NULL,
+                display_name VARCHAR(256) NOT NULL
+            );
+        
+            CREATE TABLE favorites (
+                id VARCHAR(64) PRIMARY KEY,
+                -- your data columns
+            );
+        `);
+
+        console.log('create tables complete');
+    }
+    catch (err) {
+        // problem? let's see the error...
+        console.log(err);
+    }
+    finally {
+        // success or failure, need to close the db connection
+        client.end();
+    }
+    
+}
